fa77fc9296d57da397ca30aec6cefc41b8a2ea6d
masawo9409 <masawo9409@gmail.com>
2022-11-16 01:46:16 +0900

    seeder差し込み成功

--- database/seeders/Planet_Seeder.php
+++ database/seeders/Planet_Seeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Fecades\DB;
+use Illuminate\Support\Facades\DB;
 
 class Planet_Seeder extends Seeder
 {
@@ -14,54 +14,70 @@ class Planet_Seeder extends Seeder
      */
     public function run()
     {
-        $param=[
+        $param = [
             [
-                'planet_name'=>'水星',
-                'english_name'=> 'MERCURY',
-                'radius'=> 2439,
-                'weight'=> 3310
+                'planet_name' => '水星',
+                'english_name' => 'MERCURY',
+                'radius' => 2439,
+                'weight' => 3310,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
             ],
             [
-                'planet_name'=>'金星',
-                'english_name'=> 'VENUS',
-                'radius'=> 6052,
-                'weight'=> 48700
+                'planet_name' => '金星',
+                'english_name' => 'VENUS',
+                'radius' => 6052,
+                'weight' => 48700,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
             ],
             [
-                'planet_name'=> '地球',
-                'english_name'=> 'EARTH',
-                'radius'=> 6378,
-                'weight'=> 59760
+                'planet_name' => '地球',
+                'english_name' => 'EARTH',
+                'radius' => 6378,
+                'weight' => 59760,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
             ],
             [
-                'planet_name'=> '火星',
-                'english_name'=> 'MARS',
-                'radius'=> 3397,
-                'weight'=> 6420
+                'planet_name' => '火星',
+                'english_name' => 'MARS',
+                'radius' => 3397,
+                'weight' => 6420,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
             ],
             [
-                'planet_name'=>'木星',
-                'english_name'=> 'JUPITER',
-                'radius'=> 71492,
-                'weight'=> 18993500
+                'planet_name' => '木星',
+                'english_name' => 'JUPITER',
+                'radius' => 71492,
+                'weight' => 18993500,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
             ],
             [
-                'planet_name'=> '土星',
-                'english_name'=> 'SATURN',
-                'radius'=> 60268,
-                'weight'=> 5685980
+                'planet_name' => '土星',
+                'english_name' => 'SATURN',
+                'radius' => 60268,
+                'weight' => 5685980,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
             ],
             [
-                'planet_name'=> '天王星',
-                'english_name'=> 'URANUS',
-                'radius'=> 25559,
-                'weight'=> 868910
+                'planet_name' => '天王星',
+                'english_name' => 'URANUS',
+                'radius' => 25559,
+                'weight' => 868910,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
             ],
             [
-                'planet_name'=> '海王星',
-                'english_name'=> 'NEPTUNE',
-                'radius'=> 24764,
-                'weight'=> 1029660
+                'planet_name' => '海王星',
+                'english_name' => 'NEPTUNE',
+                'radius' => 24764,
+                'weight' => 1029660,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
             ]
         ];
         DB::table('planets')->insert($param);
